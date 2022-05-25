cf09eeae2001786533eec20b057075a02ad89af4
KumuduS <47547557+KumuduS@users.noreply.github.com>
2022-05-26 00:33:57 +0530

    Initial commit

---
+++ app/src/main/java/co/devskills/backengboilerplate/BackengBoilerplateApplication.java
@@ -0,0 +1,13 @@
+package co.devskills.backengboilerplate;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackengBoilerplateApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BackengBoilerplateApplication.class, args);
+	}
+
+}
